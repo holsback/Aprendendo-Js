a42896b433857595ea6d10aebb260a9a9ef7ba12
AIPSI01T <aipsi01t@LABSSR.LOCAL>
2019-07-03 15:10:23 -0300

    Algoritimos

--- Aprendendo.js
+++ Aprendendo.js
@@ -1,10 +1,13 @@
 console.log('Teste....')
 
+//Para
 var i;
 for(i = 1; i <= 10; i ++){
     console.log(i);
 }
 
+
+//Escolha caso
 var diaDaSemana = 4;
 switch (diaDaSemana) {
     case 1:
@@ -32,6 +35,8 @@ switch (diaDaSemana) {
         console.log("Esse dia não é válido")
 }
 
+
+//se e senao
 var name = "Bruno"
 var idade = 14
 if(idade <= 18) {
@@ -40,12 +45,14 @@ if(idade <= 18) {
     console.log(name + " Maior de idade")
 }
 
+//Enquanto
 num = 0;
 while (num <= 10) {
     console.log(num);
     num++;
 }
 
+//Faça enquanto
 var numero = 0;
 do {
     numero ++
