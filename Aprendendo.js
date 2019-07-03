18277ae7f68ee2a7630a0a2c2a5aa7b657267ea9
AIPSI01T <aipsi01t@LABSSR.LOCAL>
2019-07-03 14:31:38 -0300

    Algaritimo com se

--- Aprendendo.js
+++ Aprendendo.js
@@ -5,3 +5,12 @@ for(i = 1; i <= 10; i ++){
     console.log(i);
 }
 
+sexo = "feminino";
+ 
+switch (sexo) {
+    case "feminino":
+        console.log("Bem-vinda!");
+        break;
+    case false:
+        console.log("Bem-vindo!");
+} 
